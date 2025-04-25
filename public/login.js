73e545fcd92af945e606f6f860a31b74868dbd08
sushmitadamarla <158757381+sushmitadamarla@users.noreply.github.com>
2025-04-25 19:48:44 +0530

    Add files via upload

---
+++ public/login.js
@@ -0,0 +1,6 @@
+document.getElementById("loginForm").addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    // Redirect to the main website
+    window.location.href = "index.html"; // Replace 'index.html' with the actual path to your main website page
+});
